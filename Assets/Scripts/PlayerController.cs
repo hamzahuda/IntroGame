20b8fafc34cadcd3cd7d03e084198d1018ab868d
Hamza Huda <116317495+hamzahuda@users.noreply.github.com>
2024-10-07 21:02:57 +0100

    Add pickups functionality

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour {
 
     public Vector2 moveValue;
     public float speed;
+    private int count;
+
+    void Start()
+    {
+        count = 0;
+    }
 
     void OnMove(InputValue value) {
         moveValue = value.Get<Vector2>();
@@ -16,5 +22,14 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveValue.x, 0.0f, moveValue.y);
 
         GetComponent<Rigidbody>().AddForce(movement * speed * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count++;
+        }
     }
 }
